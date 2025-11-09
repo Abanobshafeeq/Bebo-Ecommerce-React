7d48dea98dd2d0fad62e86457772aab862acc652
Abanobshafeeq <https://github.com/Abanobshafeeq>
2025-11-09 17:12:01 +0200

    upload responcive css

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,92 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import { IoMdMenu } from "react-icons/io";
-import { MdOutlineArrowDropDown } from "react-icons/md";
+import { useEffect, useState } from 'react';
+import { FaUserPlus } from 'react-icons/fa6';
+import { IoMdMenu } from 'react-icons/io';
+import { MdOutlineArrowDropDown } from 'react-icons/md';
+import { PiSignInBold } from 'react-icons/pi';
 import { Link, useLocation } from 'react-router-dom';
-import { PiSignInBold } from "react-icons/pi";
-import { FaUserPlus } from "react-icons/fa6";
-
 
 const NavLinks = [
-  {title: "Home" , link : "/"},
-  { title: "About", link: "/about" },
-  { title: "Accessories", link: "/accessories" },
-  { title: "Blog", link: "/blog" },
-  { title: "Contact", link: "/contact" },
-]
+  { title: 'Home', link: '/' },
+  { title: 'About', link: '/about' },
+  { title: 'Accessories', link: '/accessories' },
+  { title: 'Blog', link: '/blog' },
+  { title: 'Contact', link: '/contact' },
+];
 
 function BtmHeader() {
-
-  const location = useLocation()
+  const location = useLocation();
   const [categories, setCategories] = useState([]);
-
-  const [isCategoryOpen, setIsCategoryOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
 
   useEffect(() => {
-    setIsCategoryOpen(false)
-  },[location])
+    setIsMenuOpen(false);
+    setIsCategoryOpen(false);
+  }, [location]);
 
   useEffect(() => {
     fetch('https://dummyjson.com/products/categories')
-    .then((res) => res.json())
-    .then((data) => setCategories(data))
-  }, [])
-
-  
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
 
   return (
-  <div className="btm_header">
-    <div className="container">
-      {/* Hamburger icon visible on mobile */}
-      {/* <div className="mobile_menu_icon" onClick={() => setIsCategoryOpen(!isCategoryOpen)}>
-        <IoMdMenu />
-      </div> */}
+    <div className="btm_header">
+      <div className="container">
 
-      <nav className={`nav ${isCategoryOpen ? "active" : ""}`}>
-        <div className="category_nav">
-          <div
-            className="category_btn"
-            onClick={() => setIsCategoryOpen(!isCategoryOpen)}
-          >
-            <IoMdMenu />
-            <p>Browse Category</p>
-            <MdOutlineArrowDropDown />
-          </div>
-
-          <div
-            className={`category_nav_list ${isCategoryOpen ? "active" : ""}`}
-          >
-            {categories.map((category) => (
-              <Link key={category.slug} to={`category/${category.slug}`}>
-                {category.name}
-              </Link>
-            ))}
-          </div>
+        <div
+          className="mobile_menu_icon"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <IoMdMenu />
+          <span style={{color:'white'}}>Menu</span>
         </div>
 
-        <div className="nav_links">
-          {NavLinks.map((item) => (
-            <li
-              key={item.link}
-              className={location.pathname === item.link ? "active" : ""}
+        <nav className={`nav ${isMenuOpen ? 'active' : ''}`}>
+
+          <div className="category_nav">
+            <div
+              className="category_btn"
+              onClick={() => setIsCategoryOpen(!isCategoryOpen)}
             >
-              <Link to={item.link}>{item.title}</Link>
-            </li>
-          ))}
-        </div>
-      </nav>
+              <IoMdMenu />
+              <p>Browse Category</p>
+              <MdOutlineArrowDropDown />
+            </div>
 
-      <div className="sign_regs_icon">
-        <Link to="/">
-          <PiSignInBold />
-        </Link>
-        <Link to="/">
-          <FaUserPlus />
-        </Link>
+            <div
+              className={`category_nav_list ${isCategoryOpen ? 'active' : ''}`}
+            >
+              {categories.map((category) => (
+                <Link key={category.slug} to={`/category/${category.slug}`}>
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <ul className="nav_links">
+            {NavLinks.map((item) => (
+              <li
+                key={item.link}
+                className={location.pathname === item.link ? 'active' : ''}
+              >
+                <Link to={item.link}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {/* أيقونات الدخول */}
+        <div className="sign_regs_icon">
+          <Link to="/">
+            <PiSignInBold />
+          </Link>
+          <Link to="/">
+            <FaUserPlus />
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
 }
 
-export default BtmHeader
+export default BtmHeader;
--- src/components/slideProdect/SlideProduct.jsx
+++ src/components/slideProdect/SlideProduct.jsx
@@ -5,12 +5,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Product from './Product';
 import './slideProduct.css';
 
-function SlideProduct({data , title}) {
+function SlideProduct({ data, title }) {
   return (
     <div className="slide_product slide">
       <div className="container">
         <div className="top_slide">
-          <h2>{title } </h2>
+          <h2>{title} </h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
             voluptates?
@@ -20,21 +20,30 @@ function SlideProduct({data , title}) {
           loop={true}
           autoplay={{
             delay: 2500,
-            disableOnInteraction: false,
+            disableOnInteraction: true,
           }}
-          slidesPerView={5}
           navigation={true}
+          slidesPerView={5}
+          spaceBetween={10}
+          breakpoints={{
+            1200: { slidesPerView: 5 },
+            992: { slidesPerView: 4 },
+            768: { slidesPerView: 3 },
+            480: { slidesPerView: 2 },
+            300: { slidesPerView: 2 },
+          }}
           modules={[Navigation, Autoplay]}
           className="mySwiper"
         >
-
-            {data.map((item)=>{
-                return(
-                <SwiperSlide> <Product item={item} /></SwiperSlide>
-            )})}
-         
+          {data.map((item) => {
+            return (
+              <SwiperSlide>
+                {' '}
+                <Product item={item} />
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
-        
       </div>
     </div>
   );
